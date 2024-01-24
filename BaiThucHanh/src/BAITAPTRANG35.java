3a8742b72bf4f00ceb9981acd3f90963d4d5fc1a
TranHuyHoang69 <tranhuyhoangwork69@gmail.com>
2024-01-24 14:32:38 +0700

    BaiThucHanh

---
+++ BaiThucHanh/src/BAITAPTRANG35.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BAITAPTRANG35 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap so thu nhat : ");
+        double a = sc.nextDouble();
+        System.out.println("Nhap so thu hai");
+        double b = sc.nextDouble();
+        if(a<b){
+            System.out.println("So lon nhat la : "+ b);
+        }else{
+                System.out.println("So lon nhat la : " + a);
+            }
+        sc.close();
+        }
+    }
